041cba17a849ee6d54a264d7907792aada0e1c94
Hanan Abed Al Kareem Mohammed Abo Lo Ly <hp@DESKTOP-MNGGFRQ>
2025-05-18 19:17:06 +0300

    Login form

---
+++ ValidationDemo/Controllers/AccountController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ValidationDemo.Models;
+
+namespace ValidationDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        //Get:/Account/Login
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //Post: /Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login (UserLogin model)
+            {
+            if (ModelState.IsValid)
+            {
+                return View(model);
+            }
+            return RedirectToAction("Login Success");
+        }
+
+        //Get: /Account /Login Success
+        [HttpGet]
+        public IActionResult LoginSuccess()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ValidationDemo/Models/UserLogin.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ValidationDemo.Models
+{
+    public class UserLogin
+    {
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(50,MinimumLength =3 ,ErrorMessage ="Username must be between 3 and 50 characters")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Passwored is required")]
+        [DataType(DataType.Password)]
+        [StringLength(100,MinimumLength =6, ErrorMessage = "Passwored must be at least 6 characters")]
+        [Display(Name="UserPasswored")]
+        public string Password { get; set; }
+        
+        [Display(Name = "Remember Me")]
+        public bool Remember { get; set; }
+    }
+}
